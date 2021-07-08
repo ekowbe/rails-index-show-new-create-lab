db2f91fa7935e385f1edf762624c8fb2bed04b98
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-08 09:53:50 +0000

    index showo nnew create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+30.times do 
+    Coupon.create(coupon_code: Faker::Code.sin, store: Faker::Nation.capital_city)
+end
